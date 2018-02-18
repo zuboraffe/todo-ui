858287345a95b9be0a7b33da069a44f868b9b62b
zubudog <20251247+zubudog@users.noreply.github.com>
2018-02-18 17:29:56 +0900

    add

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;
